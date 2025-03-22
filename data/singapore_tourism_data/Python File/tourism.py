5fabe691bddb49a1f9de27b438efcd8b6824a265
Nodoka Kushioka <163884459+nodokak1@users.noreply.github.com>
2025-03-23 02:21:31 +0800

    Analysis of External Factors and Adding Datasets

--- data/singapore_tourism_data/Python File/tourism.py
+++ data/singapore_tourism_data/Python File/tourism.py
@@ -1,7 +1,7 @@
 import requests
 import pandas as pd
           
-dataset_id = "d_c6d3f73a65d1fa3a92f1f4e70e619a66"
+dataset_id = "d_7e7b2ee60c6ffc962f80fef129cf306e"
 url = "https://data.gov.sg/api/action/datastore_search?resource_id="  + dataset_id 
         
 response = requests.get(url)
@@ -11,5 +11,8 @@ df = df.drop(columns = ['_id'])
 colnames = list(df)
 colnames = colnames[1:]
 df = pd.melt(df, id_vars = ['DataSeries'], value_vars = colnames, var_name = 'year_month', value_name = 'no_of_visitors')
+df['DataSeries'] = df['DataSeries'].str.lstrip()
+df['year_month'] = pd.to_datetime(df['year_month'], format = '%Y%b')
+df.columns = ['Region/Country', 'year_month', 'no_of_visitors']
 
 df.to_csv('data/singapore_tourism_data/Final/tourism.csv', index=False)
